5774bbbec543c2b2220c616495aedf29d93028be
Nikunj Patel <86245332+nikunjp1272@users.noreply.github.com>
2026-01-07 02:25:59 +0530

    Update EKS&KubeAdmin.sh

--- AWSAdminTasks/EKS&KubeAdmin.sh
+++ AWSAdminTasks/EKS&KubeAdmin.sh
@@ -1,20 +1,20 @@
-# run in clodushell or in the ubuntu env for a profile called LAB_P
-aws eks update-kubeconfig --region eu-west-1 --name cluster-name --profile LAB_P
+# run in clodushell or in the ubuntu env for a profile called LAB
+aws eks update-kubeconfig --REGION eu-west-1 --name CLUSTER-NAME --profile LAB
 # run in clodushell or in the ubuntu env for a profile called DEV_PROFILE
-aws eks update-kubeconfig --region us-east-1 --name dev-cluster --profile DEV_PROFILE
+aws eks update-kubeconfig --REGION us-east-1 --name dev-cluster --profile DEV_PROFILE
 # to assume a role in AWS
-aws sts assume-role --role-arn "arn:aws:iam::722052991222:role/engieit-bis-prod.PowerUserAccess" --role-session-name AWSCLI-Session
+aws sts assume-role --role-arn "arn:aws:iam::123456789:role/IAMRoleName" --role-session-name AWSCLI-Session
 # to check current role for a certain profile
-aws sts get-caller-identity --profile LAB_P
+aws sts get-caller-identity --profile LAB
 # to fetch token manually
-aws eks get-token --region eu-west-1 --cluster-name common-lab --profile LAB_P
-# list clusters in a region
-aws eks list-clusters --region <region>
+aws eks get-token --REGION eu-west-1 --CLUSTER-NAME common-lab --profile LAB
+# list clusters in a REGION
+aws eks list-clusters --REGION <REGION>
 # get info about a cluster
-aws eks describe-cluster --name <cluster-name> --region <region>
+aws eks describe-cluster --name <CLUSTER-NAME> --REGION <REGION>
 
 # login to the pod
-kubectl exec -it -n wm-cristal <name-of-pod> -- bash
+kubectl exec -it -n <NAMESPACE> <POD-NAME> -- bash
 
 # to return all pods
 kubectl get pods -A
@@ -29,14 +29,14 @@ kubectl get pods --all-namespaces
 # watch pods in real-time
 kubectl get pods --all-namespaces --watch
 # describe a specific pod
-kubectl describe pod <pod-name> -n <namespace>
+kubectl describe pod <pod-name> -n <NAMESPACE>
 # view pod logs
-kubectl logs <pod-name> -n <namespace>
-kubectl logs <pod-name> -c <container-name> -n <namespace>
+kubectl logs <pod-name> -n <NAMESPACE>
+kubectl logs <pod-name> -c <container-name> -n <NAMESPACE>
 # view logs of previous pod instance (after crash)
-kubectl logs <pod-name> -p -n <namespace>
+kubectl logs <pod-name> -p -n <NAMESPACE>
 # check pod resource usage
-kubectl top pod -n <namespace>
+kubectl top pod -n <NAMESPACE>
 kubectl get pods --all-namespaces --watch
 watch kubectl get pods -A
 
@@ -54,7 +54,7 @@ kubectl describe node <node-name>
 # check available contexts
 kubectl config get-contexts
 # rename contexts
-kubectl config rename-context arn:aws:eks:eu-west-1:123456789:cluster/cluster-name lab
+kubectl config rename-context arn:aws:eks:eu-west-1:123456789:cluster/CLUSTER-NAME lab
 kubectl config rename-context arn:aws:eks:us-east-1:987654321:cluster/clluster-name dev
 # use contexts
 kubectl config use-context lab
@@ -63,13 +63,13 @@ kubectl config use-context dev
 # list all deployments in all namespaces
 kubectl get deployments --all-namespaces
 # Get resource as YAML
-kubectl get deployment <DEPLOYMENT_NAME> -n <NAMESPACE> -o yaml > tmp.yaml
+kubectl get deployment <DEPLOYMENT-NAME> -n <NAMESPACE> -o yaml > tmp.yaml
 # get deployments and their statuses
-kubectl get deployments -n <namespace>
+kubectl get deployments -n <NAMESPACE>
 # get deployments for all namespaces
 kubectl get deployments --all-namespaces
 # check rollout status of a deployment
-kubectl rollout status deployment/<deployment-name> -n <namespace>
+kubectl rollout status deployment/<DEPLOYMENT-NAME> -n <NAMESPACE>
 
 # get all namespaces
 kubectl get namespaces
@@ -96,17 +96,17 @@ kubectl get configmaps -n <NAMESPACE>
 kubectl get secrets -n <NAMESPACE>   # secrets are base64-encoded
 kubectl get all -n <NAMESPACE>
 kubectl get $(kubectl api-resources --namespaced=true -o name | tr '\n' ',' ) -n <NAMESPACE> #can be heavy
-kubectl api-resources --namespaced=true
+kubectl api-resources --NAMESPACEd=true
 
 # extracting yamls
-kubectl get deployment <DEPLOYMENT_NAME> -n <NAMESPACE> -o yaml
-kubectl get pod <POD_NAME> -n <NAMESPACE> -o yaml
+kubectl get deployment <DEPLOYMENT-NAME> -n <NAMESPACE> -o yaml
+kubectl get pod <POD-NAME> -n <NAMESPACE> -o yaml
 
 # check what can u do in a namespace
 kubectl auth can-i --list -n <NAMESPACE>
 
 # view recent events in a namespace
-kubectl get events -n <namespace> --sort-by=.metadata.creationTimestamp
+kubectl get events -n <NAMESPACE> --sort-by=.metadata.creationTimestamp
 
 # list all failing pods
 kubectl get pods --all-namespaces --field-selector=status.phase!=Running,status.phase!=Succeeded
